0f7c8b2b260de5e657f96b7e2f60286599f5e4b2
Pedro Magalhães e Silva <pedro.m.silva@aitec.pt>
2024-06-07 12:51:10 +0100

    updated git cloning

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -182,7 +182,7 @@ end
 
 #global_opts = %w[-c http.extraheader="AUTHORIZATION: Basic #{ENV["PAT_B64"]}"]
 #git = Git.clone("https://#{ENV["PAT_B64"]}@devops.aitec.pt/TFS2013_Migrated/PROD_EDOC4SP/_git/edoclink-service")
-git = `git clone https://#{ENV["PAT_B64"]}@devops.aitec.pt/TFS2013_Migrated/PROD_EDOC4SP/_git/edoclink-service`
+git = `git clone https://{#{ENV["PAT_B64"]}}@devops.aitec.pt/TFS2013_Migrated/PROD_EDOC4SP/_git/edoclink-service`
 
 ##############################
 # Fetch the dependency files #
