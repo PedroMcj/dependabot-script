5db9cb083907abc450bd33460b6f03c3f7e1f4c5
Pedro Magalhães e Silva <pedro.m.silva@aitec.pt>
2024-06-07 10:56:26 +0100

    updated git cloning

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -172,7 +172,12 @@ else
   )
 end
 
-git = Git.clone("https://pedro.m.silva:#{ENV["AZURE_PWD"]}@devops.aitec.pt/TFS2013_Migrated/PROD_EDOC4SP/_git/edoclink-service")
+# Clone the repo for the current project
+
+git = `git -c http.extraheader="AUTHORIZATION: Basic #{ENV["PAT_B64"]}" clone https://devops.aitec.pt/TFS2013_Migrated/PROD_EDOC4SP/_git/edoclink-service`
+
+#global_opts = %w[-c http.extraheader="AUTHORIZATION: Basic #{ENV["PAT_B64"]}"]
+#git = Git.clone("https://pedro.m.silva:#{ENV["AZURE_PWD"]}@devops.aitec.pt/TFS2013_Migrated/PROD_EDOC4SP/_git/edoclink-service")
 
 
 ##############################
